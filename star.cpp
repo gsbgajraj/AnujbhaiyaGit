2e2da0f747b0ba9d7775a7d8b3cf991928b651b5
Gajraj Singh Bhati <gsbgajraj@gmail.com>
2024-01-26 15:31:08 +0530

    all files commit

--- star.cpp
+++ star.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello world";
+    cout<<"hello world\n";
+    int i=1;
+    while(i!=10)
+    {
+        cout<<"*";
+        i++;
+    }
     return 0;
 }
