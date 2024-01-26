16e9c56f81948383bef688ca68a23433fd236263
Gajraj Singh Bhati <gsbgajraj@gmail.com>
2024-01-26 15:28:52 +0530

    function wala program c++ me

---
+++ function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello world\n";
+    int i=1,j=1;
+    while(i!=10)
+    {
+        while(j!=5)
+        {
+          cout<<"*"<<" ";
+        j++;
+      }
+        i++;
+    }
+    return 0;
+}
